6e608c0bdacb08e3315117fa8e84a61702d2fcdc
Francisco Sokol <chico.sokol@gmail.com>
2013-05-22 11:52:10 -0300

    reading commit objects properly in test case

--- src/test/java/br/com/caelum/zhit/model/CommitObjectTest.java
+++ src/test/java/br/com/caelum/zhit/model/CommitObjectTest.java
@@ -2,8 +2,12 @@ package br.com.caelum.zhit.model;
 
 import static org.junit.Assert.fail;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.util.zip.InflaterInputStream;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
 public class CommitObjectTest {
@@ -17,5 +21,19 @@ public class CommitObjectTest {
 		fail();
 		//TODO: asserts
 	}
-
+	
+	@Test
+	public void escovandoBytes() throws Exception {
+		File dotGit = new File("src/test/resources/sample-repository/.git/objects/25/0f67ef017fcb97b5371a302526872cfcadad21");
+		InflaterInputStream inflaterInputStream = new InflaterInputStream(new FileInputStream(dotGit));
+		Integer read = inflaterInputStream.read();
+		while(read != 0) {
+			read = inflaterInputStream.read();
+			System.out.println((char)read.byteValue());
+		}
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
+		IOUtils.copyLarge(inflaterInputStream, os);
+		System.out.println(new String(os.toByteArray()));
+	}
+	
 }
